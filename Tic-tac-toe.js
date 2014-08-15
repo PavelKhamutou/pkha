d2631bcf5205cb7328e6bc70c1a3654bdf0434ff
Pavel Khamutou <7upecb@gmail.com>
2014-08-15 16:44:59 +0300

    day22

--- Tic-tac-toe.js
+++ Tic-tac-toe.js
@@ -13,25 +13,101 @@ function getNames(){
     
     //setting names
     var textAdd = "Name: " + game.name1;
-    document.getElementById("player1").innerHTML = textAdd;
+    document.getElementById("player1Name").innerHTML = textAdd;
     textAdd = "Name: " + game.name2;
-    document.getElementById("player2").innerHTML = textAdd;
+    document.getElementById("player2Name").innerHTML = textAdd;
+}
+
+function drawFigure(canID){
+    if(game.turn == true){
+        alert(canID);
+        var c = document.getElementById(canID);
+        var ctx = c.getContext("2d");
+        ctx.moveTo(10, 10);
+        ctx.lineTo(180, 180);
+        ctx.stroke();
+        ctx.moveTo(180, 10);
+        ctx.lineTo(10, 180);
+        ctx.stroke();
+    }
+    else{
+        var c = document.getElementById(canID);
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(95,95,90,0,2*Math.PI);
+        ctx.stroke();
+    }
 }
 
 
 
-
-function checkOrder() {
-    alert('hello');
-    //var c = document.getElementById("canOne");
-    var c = document.getElementsByTagName("canvas");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(10, 10);
-    ctx.lineTo(180, 180);
-    ctx.stroke();
-    ctx.moveTo(180, 10);
-    ctx.lineTo(10, 180);
-    ctx.stroke();
-    
-    
+function checkOrder(cell) {
+    if(game.turn == true){
+        alert(cell);
+        switch (cell){
+        case "one":
+                drawFigure("can1");
+                break;
+        case "two":
+                drawFigure("can2");
+                break;
+        case "three":
+                drawFigure("can3");
+                break;
+        case "four":
+                drawFigure("can4");
+                break;
+        case "five":
+                drawFigure("can5");
+                break;
+        case "six":
+                drawFigure("can6");
+                break;
+        case "seven":
+                drawFigure("can7");
+                break;
+        case "eight":
+                drawFigure("can8");
+                break;
+        case "nine":
+                drawFigure("can9");
+                break;
+        
+        }
+        game.turn = false;
+    }
+    else{
+        alert("yono")
+        switch (cell){
+        case "one":
+                drawFigure("can1");
+                break;
+        case "two":
+                drawFigure("can2");
+                break;
+        case "three":
+                drawFigure("can3");
+                break;
+        case "four":
+                drawFigure("can4");
+                break;
+        case "five":
+                drawFigure("can5");
+                break;
+        case "six":
+                drawFigure("can6");
+                break;
+        case "seven":
+                drawFigure("can7");
+                break;
+        case "eight":
+                drawFigure("can8");
+                break;
+        case "nine":
+                drawFigure("can9");
+                break;
+        
+        }
+        game.turn = true;
+    }
 }
